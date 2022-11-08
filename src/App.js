901632e4f4af6174474052da6f32d264746a11b1
Willians <williansaaid@gmail.com>
2022-11-08 17:51:24 -0500

    Package.json & Routes(src/App.js) fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
-import ArtistForm from "./Components/ArtistForm/ArtistForm";
 import { Provider } from "react-redux"
 import store from "./Redux"
+
+//Components Import
+import ArtistForm from "./Components/ArtistForm/ArtistForm";
+import Home from "./Components/Home/Home";
+
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <Routes>
           <Route exact path={'/login/artista'} element={<ArtistForm/>}/>
+          <Route path={"/"} element={<Home/>}/>
         </Routes>
       </Provider>
     </div>
   );
-=======
-import "./App.css";
-import Home from "./Components/Home/Home";
-
-function App() {
-	return (
-		<>
-			<Home />
-		</>
-	);
->>>>>>> 015ed05e9cabe7730195c742f0a1e49b2f1087f3
 }
 
 export default App;
