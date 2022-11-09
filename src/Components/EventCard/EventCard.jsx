a40ffc8ba7ac72bd8371f6fa9043a89814697baf
martinsosafer <martin_sos@gmail.com>
2022-11-09 14:03:27 -0300

    Cards Events

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -5,36 +5,24 @@ import { useSelector } from "react-redux";
 
 export const Events = ()=> {
   const dispatch =useDispatch()
+  const {events} = useSelector(state=>state.events)
+
   useEffect(()=>{
     
     dispatch(getEvents())
   },[])
-  const events = useSelector(state=>state.events)
-  console.log(events)
-  // return ( function eventCard({
-  //   name,
-  //   date,
-  //   image,
-  //   price,
-  //   id
-  // }) {
-    return (
-      <div class="eventCard">
-  <h3>hola</h3>
+  
 
-{/* <img
-  src={image ? image : "no se encontro imagen"}
-  alt="Img not found"
-  width="300px"
-  height="200px"
-/>2
-<h5 class="date">📅 Fecha: {date}</h5>
-<h5 class="price"> Precio:{price}</h5>
-  <a id="MoreInfo" href={`/details/${id}`}>Mas info ➡️</a>
-         */}
-      </div>
-    )
-    
+  return(
+    <div>
+    {events&&events.map((el,id)=>{
+      return(
+        <div ket={id}>
+<p>{el.name}</p>
+        </div>
+      )
+    })}
+    </div>
+  )
+     
   }
- 
- 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import CarouselCustom from "../Carousel/Carousel_custom";
 import { Events } from "../EventCard/EventCard";
-
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+  const {allEvents} = useSelector((state) => state.events);
   
+  // const indexOfLastEvent = currentPage * eventPerPage;
+  // const indexOfFirstEvent = indexOfLastEvent- eventPerPage;
+  // const currentEvent= allEvents.slice(
+  //   indexOfFirstEvent,
+  //   indexOfLastEvent
+  // );
   return(
   <div>
    <CarouselCustom />;
-
-    <Events/>
+    
+   <Events/>
   </div>
+ 
+
+ 
+ 
   )
 };
 
