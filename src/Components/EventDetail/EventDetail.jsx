1f15615a4499ec0c62d42f7e44be281d43aa5c3b
diamecjs <diame.cjs@gmail.com>
2022-11-08 23:46:12 -0500

    cambios detail

--- src/Components/EventDetail/EventDetail.jsx
+++ src/Components/EventDetail/EventDetail.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React,{useEffect  } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import {getEventsById}from "../../Redux/eventActions";
+import Navbar from "../Navbar/Navbar"
 
 
-export default function Detail(props){
 
+export default function Detail(){
+    const dispatch = useDispatch()
+    const {detail} = useSelector(state=>state.events)
 
+    // useEffect(()=>{
+    //     dispatch(getEventsById())
+    // },[])
+    return(
+        <div>
+            <Navbar/>
+        </div>
+       
+    )
 }
