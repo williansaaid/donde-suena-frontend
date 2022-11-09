8ed140a8888423ab3504c0e7b176067be79a7e9d
MartinCanolik <martin.canolik@gmail.com>
2022-11-09 20:38:47 -0300

    form usuario validacion terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux"
-import store from "./Redux"
+import { Provider } from "react-redux";
+import store from "./Redux";
 
 //Components Import
 import ArtistForm from "./Components/ArtistForm/ArtistForm";
 import Home from "./Components/Home/Home";
-import EventDetail from "./Components/EventDetail/EventDetail.jsx"
+import EventDetail from "./Components/EventDetail/EventDetail.jsx";
+import UserForm from "./Components/UserForm/UserForm";
 
 function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <Routes>
-          <Route exact path={'/login/artista'} element={<ArtistForm/>}/>
-          <Route path={"/"} element={<Home/>}/>
-          <Route exact path={'/details/:id'} element={<EventDetail/>}/>
-        </Routes>
-      </Provider>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Provider store={store}>
+				<Routes>
+					<Route exact path={"/login/artista"} element={<ArtistForm />} />
+					<Route exact path={"/login/user"} element={<UserForm />} />
+					<Route path={"/"} element={<Home />} />
+					<Route exact path={"/details/:id"} element={<EventDetail />} />
+				</Routes>
+			</Provider>
+		</div>
+	);
 }
 
 export default App;
