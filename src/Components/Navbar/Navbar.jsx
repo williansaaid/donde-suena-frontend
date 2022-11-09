893b12890bff90b6d5fc334cdce8709bdaed5a35
EzequielRamirez <ramirez.ezequielagustin@gmail.com>
2022-11-09 17:27:07 -0300

    Nav finish falta logica

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
-import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className="nav">
-
-      <div className="navLogo">
-        <img src= {require ("../../assets/img/HenryLogo.png")} alt="logo" />
+    <nav class="bg-zinc-900 py-2 relative">
+      <div class="container mx-auto flex justify-between items-center">
+        <img
+          class="h-20"
+          src={require(`../../assets/img/HenryLogo.png`)}
+          alt="logo"
+        />
+        <div class="flex items-center">
+          <div class="my-9">
+            <SearchBar></SearchBar>
+          </div>
+          <div class="text-orange-100 bg-red-600 rounded-lg ml-10 items-center p-2 flex h-10 gap-3">
+            <img
+              class="h-full"
+              src={require(`../../assets/img/HenryCuenta.png`)}
+              alt=""
+            />
+            <a href="#">Mi Cuenta</a>
+          </div>
+        </div>
       </div>
-
-      <div className="divButtonSearch">
-      <div className="SearchBar">
-        <SearchBar></SearchBar>
-      </div>
-
-      <div className="divButton">
-        <button>Mi Cuenta</button>
-      </div>
-
-      </div>
-    </div>
+    </nav>
   );
 }
 
