c3cbeeb116f6f9adad66aba4856740e22b9d4f2f
Willians <williansaaid@gmail.com>
2022-11-10 14:28:43 -0500

    ArtistForm styled

--- src/Components/UserForm/UserForm.jsx
+++ src/Components/UserForm/UserForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { useState } from "react";
-import "./UserForm.css";
+// import "./UserForm.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
