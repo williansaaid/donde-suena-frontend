8943532080b37db1b4d414e68f0b97be87e7b3bb
diamecjs <diame.cjs@gmail.com>
2022-11-13 20:39:35 -0500

    detail modificado

--- src/Components/EventDetail/EventDetail.jsx
+++ src/Components/EventDetail/EventDetail.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getEventsById } from "../../Redux/eventActions";
 import { Link } from "react-router-dom";
+import CarouselRec from "../CarouselRec/CarouselRec";
 
 const EventDetail = (props) => {
     const dispatch = useDispatch();
@@ -16,34 +17,36 @@ const EventDetail = (props) => {
     return (
         <section class="text-gray-700 body-font overflow-hidden bg-white">
             <div class="lg:w-3/1 mx-1 flex">
-                <div class="object-contain  w-150 mt-6 space-y-4">
-                  <div class= "relative">
+                <div class="object-contain rounded-lg  w-150 mt-6 space-y-4">
+                    <div class="relative rounded-lg">
+                        <img
+                            alt="event"
+                            class=" object cover rounded-lg border-gray-200 px-8 border-transparent"
+                            src={detail.image}
+                        />
+                        <div class="absolute bottom-0 left-8 right-8 px-10 py-12 bg-gray-800 opacity-70">
+                            <div class="mb-6">
+                                <h2 class="absolute bottom-10  ml-15 text-sm title-font text-white tracking-widest">
+                                    {detail.date}
+                                </h2>
+                                <h1 class="absolute mb-4 bottom-12 ml-15 text-white text-3xl title-font font-medium mb-1">
+                                    {detail.name}
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
                     <img
                         alt="event"
-                        class=" object cover rounded border border-gray-200 px-8 border-transparent"
-                        src={detail.image}
-                    />
-                    <div class="absolute bottom-0 left-8 right-8 px-10 py-12 bg-gray-800 opacity-70">
-                      <div class="mb-6">
-                     <h2 class="absolute bottom-10  ml-15 text-sm title-font text-white tracking-widest">
-                        {detail.date}
-                    </h2>
-                    <h1 class="absolute mb-4 bottom-12 ml-15 text-white text-3xl title-font font-medium mb-1">
-                        {detail.name}
-                    </h1>
-                    </div>
-                    </div>
-                    </div>
-                    <img
-                        alt="event"
-                        class="rounded border border-gray-200 px-8 border-transparent"
+                        class="rounded-lg border-gray-200 px-8 border-transparent"
                         src="https://parabuenosaires.com/wp-content/uploads/2017/03/google-maps-bs-as.jpg"
                         height="100px"
                         width="700px"
                     />
                 </div>
-                <div class="lg:w-1/2 lg:pl-5 lg:py-10 lg:mt-6 bg-gray-300 rounded-lg">
-                 <h2 class="font-bold text-xl text-center">Nombre del concierto</h2>
+                <div class=" lg:w-1/2 lg:pl-5 lg:py-10 lg:mt-6 bg-gray-300 rounded-lg">
+                    <h2 class="font-bold text-xl text-center">
+                        Nombre del concierto
+                    </h2>
                     <p class="leading-relaxed">
                         {detail.description}arcu ac tortor dignissim convallis
                         aenean et tortor at risus viverra adipiscing at in
@@ -99,7 +102,7 @@ const EventDetail = (props) => {
                     </div>
                     <div class="grid h-15px place-items-center ">
                         <ul class="flex flex-wrap">
-                            <li class="pr-4 ... pt-40 ... px-8 ...">
+                            <li class="flex flex-wrap">
                                 {" "}
                                 <img
                                     src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668128720/Donde-Suena-Assets/thumbnail_instagram_uhwi1o.png"
@@ -130,13 +133,16 @@ const EventDetail = (props) => {
                         <ul class="flex flex-wrap items-center mt-3 text-sm text-white-500 dark:text-gray-400 sm:mt-0"></ul>
                     </div>
                     <div class="flex mx-20 my-20 mr-20 justify-center">
-                      <Link to="/">
-                        <button class=" text-white bg-customRed border-0 py-2 px-6 focus:outline-none hover:bg-red-500 rounded">
-                            Go Back!
-                        </button>
+                        <Link to="/">
+                            <button class=" text-white bg-customRed border-0 py-2 px-6 focus:outline-none hover:bg-red-500 rounded">
+                                Go Back!
+                            </button>
                         </Link>
                     </div>
                 </div>
+            </div>
+            <div>
+                <CarouselRec />
             </div>
         </section>
     );
--- src/Redux/eventSlice.js
+++ src/Redux/eventSlice.js
@@ -1,3 +1,4 @@
+import { createSlice } from "@reduxjs/toolkit";
 
 export const eventsSlice = createSlice({
 	name: "events",
