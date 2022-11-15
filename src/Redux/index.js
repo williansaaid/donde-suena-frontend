a1506a615d5830549494a1e76adeb0596049fe65
Willians <williansaaid@gmail.com>
2022-11-15 14:23:10 -0500

    Creating Dirs & Redux Slices (src/Redux)

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -2,13 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import events from "../Redux/eventSlice";
 import detail from "../Redux/eventSlice";
 import modal from "../Redux/eventSlice";
-import places from "../Redux/Slices/Places/placesSlice"
+import places from "../Redux/Slices/Places/placesSlice";
+import favorites from "../Redux/Slices/Favorites/favoritesSlice";
+import genres from "../Redux/Slices/Genres/genresSlice";
+import purchased from "../Redux/Slices/Purchased/purchasedSlice";
 
 export default configureStore({
     reducer: {
         events: events,
         detail: detail,
         modal: modal,
-        places: places
+        places: places,
+        favorites: favorites,
+        genres: genres,
+        purchased: purchased
     }
 });
