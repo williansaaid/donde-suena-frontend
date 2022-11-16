931b315b359f869f38272de8a5d21c3c0d35acff
Willians <williansaaid@gmail.com>
2022-11-15 23:33:04 -0500

    Component Artist Favs (Remains functional work)

--- src/Components/UserFavorites/UserFavorites.jsx
+++ src/Components/UserFavorites/UserFavorites.jsx
@@ -5,13 +5,13 @@ import { getFavorites } from "../../Redux/Slices/Favorites/favoritesAction";
 const favorites = [
     {
         id: 1,
-        name: "Juan",
+        name: "Juan y su cuarteto",
         img: "https://images.unsplash.com/photo-1526218626217-dc65a29bb444?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2luZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
         genre: "Pop"
     },
     {
         id: 2,
-        name: "Juanito",
+        name: "Bohemian",
         img: "https://images.unsplash.com/photo-1526218626217-dc65a29bb444?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2luZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
         genre: "Rap"
     },
@@ -49,16 +49,18 @@ const UserFavorites = () => {
     // }, []);
 
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center font-source-sans text-white border-2 rounded-3xl">
-            <h4 className="text-white uppercase font-bold py-4 text-xl">Tus Artistas Favoritos</h4>
+        <div className="h-full w-2/6 max-w-xs min-w-fit flex flex-col items-center justify-center font-source-sans text-white border-2 rounded-3xl bg-customGray pb-4">
+            <h4 className="text-white uppercase font-bold py-4 text-xl px-6">Tus Artistas Favoritos</h4>
             {favorites.map(artist => {
                 return (
-                    <div key={artist.id} className="w-full h-auto flex flex-col items-center justify-center gap-2 border rounded-lg font-bold">
-                        <div className="flex items-center justify-between">
-                            <p className="uppercase">{artist.name}</p>
+                    <div key={artist.id} className="w-5/6 h-auto flex justify-between items-center border rounded-3xl font-bold px-8 py-2 mb-2">
+                        <div className="flex flex-col items-start justify-between w-4/6">
+                            <p className="uppercase max-w-xs">{artist.name}</p>
+                            <p className="text-gray-400">{artist.genre}</p>
+                        </div>
+                        <div className="w-2/6 max-w-xs flex items-center justify-end">
                             <img src={artist.img} alt={`${artist.name} image`} className="w-12 h-12 rounded-full"/>
                         </div>
-                        <p className="font-">{artist.genre}</p>
                     </div>
                 )
             })}
