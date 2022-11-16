4be65175d8c941465c777b764115e9de6985e466
diamecjs <diame.cjs@gmail.com>
2022-11-16 18:12:40 -0500

    postvar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import Login from "./Components/Login/Login";
 import EventCreation from "./Components/EventForm/EventForm";
+import PostVar from "./Components/PostVar/PostVar"
 
 // import { LoginGoogle } from "./Components/Login/Login2";
 function App() {
@@ -38,6 +39,7 @@ function App() {
 
                     <Route path={"/"} element={<Home />} />
                     <Route path={"/details/:id"} element={<EventDetail />} />
+                    <Route path={"/postVar"} element={<PostVar />} />
                 </Routes>
                 <Footer />
             </div>
