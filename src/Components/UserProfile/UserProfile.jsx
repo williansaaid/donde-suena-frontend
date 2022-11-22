e0365c39e91dfa24f4769f1f8c32a99c4bb90c2f
diamecjs <diame.cjs@gmail.com>
2022-11-22 18:23:04 -0500

    perfil usuario

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -2,22 +2,58 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUserById } from "../../Redux/Slices/User/userAction";
-
+import { Link } from "react-router-dom";
 
 export default function UserProfile() {
-    const dispatch = useDispatch()
-    const { id } = useParams()
-    const { userId } = useSelector(state => state.userIdState)
-    console.log(userId)
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const { userId } = useSelector((state) => state.userIdState);
+    console.log(userId);
 
     useEffect(() => {
-        dispatch(getUserById(id))
-    }, [dispatch, id])
+        dispatch(getUserById(id));
+    }, [dispatch, id]);
 
     return (
-        <div>
-            <h1>{userId.firstName}</h1>
-            <h1>{userId.lastName}</h1>
+        <div class="mt-6 mb-6 ml-6  md:max-w-2xl break-words bg-customRed w-1/3 shadow-lg rounded-xl">
+            <div class="px-6">
+                <div class="">
+                    <div class="">
+                        <div class="relative">
+                            <img
+                                src={userId.image}
+                                class="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                            />
+                        </div>
+                    </div>
+                    <div class="w-full text-center ">
+                        <div class="h-80 lg:pt-10  ">
+                            <div class="p-3 text-center"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h3 class="text-2xl text-black font-bold leading-normal ">
+                        {userId.firstName} {userId.lastName}
+                    </h3>
+                    <div class="text-xs text-slate-400 font-bold uppercase">
+                        <i class="fas fa-map-marker-alt  text-slate-400 opacity-75"></i>
+                        {userId.email}
+                    </div>
+                </div>
+                <div class="py-6 border-t border-slate-200 text-center">
+                    <div class="flex flex-wrap justify-center">
+                        <div class=" px-4">
+                            <span class="text-xl font-bold block uppercase tracking-wide text-slate-700">
+                                <Link to="myshopping/:id">🛒</Link>
+                            </span>
+                            <span class="text-sm text-slate-400">
+                                Mis compras
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
