60ddae23045767168834169ebdb75ac21e8eec0b
diamecjs <diame.cjs@gmail.com>
2022-11-27 14:00:57 -0500

    favoritos

--- src/Components/PostVar/PostVar.jsx
+++ src/Components/PostVar/PostVar.jsx
@@ -8,7 +8,6 @@ import axios from "axios";
 export const PostVar = () => {
 
     const artistId= useSelector((state) => state.artistId);
-    console.log(artistId)
    
     const dispatch = useDispatch();
     const [image, setImage] = useState("");
