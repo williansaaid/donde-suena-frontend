21badbca27cef65aebf0c8329fa2007425defded
EzequielRamirez <ramirez.ezequielagustin@gmail.com>
2022-11-30 10:30:56 -0300

    artistProfile

--- src/Components/PostDumb/PostDumb.jsx
+++ src/Components/PostDumb/PostDumb.jsx
@@ -58,17 +58,6 @@ export const PostDumb = () => {
                             <div class="text-sm mb-2 text-gray-400 cursor-pointer font-medium">
                                 {el.comments.length}
                             </div>
-                            {/* <div class="mb-2">
-                                <div class="mb-2 text-sm">
-                                    <span class="font-medium mr-2">
-                                        razzle_dazzle
-                                    </span>{" "}
-                                    Dude! How cool! I went to New Zealand last
-                                    summer and had a blast taking the tour! So
-                                    much to see! Make sure you bring a good
-                                    camera when you go!
-                                </div>
-                            </div> */}
                         </div>
                     </div>;
                 })}
@@ -77,49 +66,3 @@ export const PostDumb = () => {
 };
 
 export default PostDumb;
-
-{
-    /* /         <div>
-    //             <div className="mt-5 w-3/4 flex items-center mx-auto rounded-lg bg-white shadow p-5 text-gray-800 bg-white shadow-lg rounded-lg mb-12 md:max-w-2xl">
-    //                 {postsId.posts &&
-                    postsId.posts?.map((el) => {
-                        return (
-                            <div className="h-50 bg-gray-200 items-center justify-center flex" >
-                                <img
-                                    class="w-12 h-12 rounded-full object-cover shadow mr-2 text-right font-bold"
-                                    src={el.image}
-                                    alt="avatar"
-                                />
-                                <h1>{postsId.nickname}</h1>
-
-                                <div class="w-full  mr-70">
-                                    <p class="text-xs text-gray-500 text-right ">
-                                        Date October 15{el.date}
-                                    </p>
-                                </div>
-                                <div>
-                                    <div className="w-full grid mb-4 p-2">
-                                        <img
-                                            className="w-52 h-16"
-                                            src={el.image ? el.image : ""}
-                                            alt="postImage"
-                                        />
-                                        <div className="grid-rows-{2}">
-                                            <div className="flex-grow pl-3 mr-10">
-                                                <div className="w-full mb-4">
-                                                    <p className="mt-3 text-gray-700 text-sm ml-3">
-                                                        {el.description}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-            </div>
-        </div>
-    );
-}; */
-}
