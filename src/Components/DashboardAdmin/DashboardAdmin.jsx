8a89b22c6eb320df5eb54a0295f988ca274b4a9c
Willians <williansaaid@gmail.com>
2022-11-30 00:57:47 -0500

    Conflicts Dashboard Admin (Verion Conflict)

--- src/Components/DashboardAdmin/DashboardAdmin.jsx
+++ src/Components/DashboardAdmin/DashboardAdmin.jsx
@@ -1,25 +1,25 @@
 import { useSelector } from "react-redux";
-import { ArtistAdmin } from "./ArtistAdmin";
-import { EventAdmin } from "./EventAdmin";
-import { UsersAdmin } from "./UsersAdmin";
-import { PlacesAdmin } from "./PlacesAdmin";
-import { Select } from "./Select";
+// import { ArtistAdmin } from "./ArtistAdmin";
+// import { EventAdmin } from "./EventAdmin";
+// import { UsersAdmin } from "./UsersAdmin";
+// import { PlacesAdmin } from "./PlacesAdmin";
+// import { Select } from "./Select";
 const DashboardAdmin = () => {
     const user = useSelector((state) => state.sessionState?.user);
 
     const isAdmin = user.admin || false;
     return (
         <div>
-            <Select />
+            {/* <Select /> */}
             <div className="flex justify-center">
                 <h1 className="text-3xl font-bold text-gray-800 dark:text-white">
                     Dashboard
                 </h1>
             </div>
-            <EventAdmin />
+            {/* <EventAdmin />
             <PlacesAdmin />
             <UsersAdmin />
-            <ArtistAdmin />
+            <ArtistAdmin /> */}
         </div>
     );
 };
