3549015f806eb001aad9afe48e22e435a7034cbb
Willians <williansaaid@gmail.com>
2022-11-30 11:28:51 -0500

    Footer Fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -20,29 +20,36 @@ export default function Footer() {
                             }
                             alt="logo"
                         />
-                        <ul className="flex flex-wrap gap-12 items-center mt-6">
-                            <li>
-                                <img
-                                    className="object-contain h-10 w-10 cursor-pointer"
-                                    src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668573857/Donde-Suena-Assets/ig-white_onltii.png"
-                                    alt="instagram logo"
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    className="object-contain h-10 w-10 cursor-pointer"
-                                    src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668574298/Donde-Suena-Assets/dlf.pt-facebook-and-twitter-icons-5924528_fq1die.png"
-                                    alt="twitter logo"
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    className="object-contain h-10 w-10 cursor-pointer"
-                                    src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668574467/Donde-Suena-Assets/pngfind.com-efectos-png-photoscape-3874615_spqlvq.png"
-                                    alt="facebook logo"
-                                />
-                            </li>
-                        </ul>
+                        <div>
+                        <div className="flex flex-col justify-center items-center mx-auto gap-4">
+                            <ul className="flex flex-wrap gap-12 items-center mt-6">
+                                <li>
+                                    <img
+                                        className="object-contain h-10 w-10 cursor-pointer"
+                                        src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668573857/Donde-Suena-Assets/ig-white_onltii.png"
+                                        alt="instagram logo"
+                                    />
+                                </li>
+                                <li>
+                                    <img
+                                        className="object-contain h-10 w-10 cursor-pointer"
+                                        src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668574298/Donde-Suena-Assets/dlf.pt-facebook-and-twitter-icons-5924528_fq1die.png"
+                                        alt="twitter logo"
+                                    />
+                                </li>
+                                <li>
+                                    <img
+                                        className="object-contain h-10 w-10 cursor-pointer"
+                                        src="https://res.cloudinary.com/ds41xxspf/image/upload/v1668574467/Donde-Suena-Assets/pngfind.com-efectos-png-photoscape-3874615_spqlvq.png"
+                                        alt="facebook logo"
+                                    />
+                                </li>
+                            </ul>
+                            <p className="text-white">
+                                © DondeSuena {new Date().getFullYear()} - Hecho con ♫ por Los Magios.
+                            </p>
+                        </div>
+                        </div>
                         <ul className="flex flex-wrap items-center text-base sm:mt-4 mr-6">
                             <li>
                                 <a href="/team" className="text-white">
@@ -51,12 +58,6 @@ export default function Footer() {
                             </li>
                         </ul>
                     </footer>
-                    <div className="flex justify-center mx-auto">
-                        <p className="text-white mb-4">
-                            © DondeSuena {new Date().getFullYear()} - Hecho con
-                            ♫ por Los Magios.
-                        </p>
-                    </div>
                 </div>
             )}
         </div>
