b9fd5cd03febeca686024edffaf000d1c256b5d9
Willians <williansaaid@gmail.com>
2022-11-30 19:36:41 -0500

    Styles & Map Commented

--- src/Components/CommentsUser/CommentForm.jsx
+++ src/Components/CommentsUser/CommentForm.jsx
@@ -37,59 +37,23 @@ const [showForm,setShowForm] =useState(false);
   //   setText("");
   // };
   return (
-<<<<<<< HEAD
-    <div className="flex mx-auto items-center justify-center shadow-lg  max-w-lg bg-gray-200 ">
-    <form onSubmit={handleSubmit}
-    className="w-full max-w-xl bg-white rounded-lg px-4 pt-2 bg-gray-200 "
-    >
-    <div className="w-full md:w-full px-3 mb-1 mt-0 ">
-
-      <div className="flex flex-wrap -mx-3 mb-3">
-
-      <textarea
-
-className="comment-form-textarea"
-className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder='Escribe tu comentario...' required
-value={text}
-onChange={handleChange}
-/>
-{ console.log(text)}
-</div>
-      <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded" disabled={isTextareaDisabled}>
-        {submitLabel}
-      </button>
-      {hasCancelButton && (
-        <button
-        type="button"
-        className="comment-form-button comment-form-cancel-button"
-
-        onClick={handleCancel}
-        >
-          Cancel
-
-        </button>
-
-        )}
-        </div>
-    </form>
-=======
 
     <div class="flex mx-auto items-center justify-center shadow-lg  max-w-lg bg-gray-200 w-screen ml-15" >
       <React.Fragment>
         {showForm? (
-        <form onSubmit={handleSubmit} 
-        class="w-full max-w-xl bg-gray-200 px-4 pt-2 bg-gray-200"
-        
-        >  
+        <form onSubmit={handleSubmit}
+        class="w-full max-w-xl bg-gray-200 px-4 pt-2"
+
+        >
         <div class="w-full md:w-full px-3 mb-1 mt-0 ">
-    
+
           <div class="flex flex-wrap -mx-3 mb-3">
-    
+
           <textarea
-    
+
     className="comment-form-textarea"
-    class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" 
-    
+    class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
+
     name="body" placeholder='Escribe tu comentario...' required
     value={text}
     onChange={handleChange}
@@ -104,22 +68,21 @@ onChange={handleChange}
             <button
             type="button"
             className="comment-form-button comment-form-cancel-button"
-            
+
             onClick={handleCancel}
             >
               Cancel
-            
+
             </button>
-          
+
             )}
             </div>
-        
+
         </form>
         ): ( <button onClick={()=> setShowForm(true) }  className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded ml-">Añadir Comentario</button>
       )}
 
     </React.Fragment>
->>>>>>> 74a2a3a6d4a6af4837b75203330309ffbfb4e58b
     </div>
   );
 };
